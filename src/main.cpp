22ec24d5b5f5d71f5843c94c2692cf45689ab653
Smiley98 <smileysofficialcinema@gmail.com>
2024-07-21 18:38:01 -0400

    Tower Defense

--- src/main.cpp
+++ src/main.cpp
@@ -1,140 +1,224 @@
-#include "raylib.h"
+#include <raylib.h>
 #include "Math.h"
 
-constexpr float SCREEN_WIDTH = 1200.0f;
-constexpr float SCREEN_HEIGHT = 800.0f;
-constexpr Vector2 CENTER{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+#include <cassert>
+#include <array>
+#include <vector>
+#include <algorithm>
 
-// Ball can move half the screen width per-second
-constexpr float BALL_SPEED = SCREEN_WIDTH * 0.5f;
-constexpr float BALL_SIZE = 40.0f;
+const float SCREEN_SIZE = 800;
 
-// Paddles can move half the screen height per-second
-constexpr float PADDLE_SPEED = SCREEN_HEIGHT * 0.5f;
-constexpr float PADDLE_WIDTH = 40.0f;
-constexpr float PADDLE_HEIGHT = 80.0f;
+const int TILE_COUNT = 20;
+const float TILE_SIZE = SCREEN_SIZE / TILE_COUNT;
 
-struct Box
+enum TileType : int
 {
-    float xMin;
-    float xMax;
-    float yMin;
-    float yMax;
+    GRASS,      // Marks unoccupied space, can be overwritten 
+    DIRT,       // Marks the path, cannot be overwritten
+    WAYPOINT,   // Marks where the path turns, cannot be overwritten
+    COUNT
 };
 
-bool BoxOverlap(Box box1, Box box2)
+struct Cell
 {
-    bool x = box1.xMax >= box2.xMin && box1.xMin <= box2.xMax;
-    bool y = box1.yMax >= box2.yMin && box1.yMin <= box2.yMax;
-    return x && y;
+    int row;
+    int col;
+};
+
+constexpr std::array<Cell, 4> DIRECTIONS{ Cell{ -1, 0 }, Cell{ 1, 0 }, Cell{ 0, -1 }, Cell{ 0, 1 } };
+
+inline bool InBounds(Cell cell, int rows = TILE_COUNT, int cols = TILE_COUNT)
+{
+    return cell.col >= 0 && cell.col < cols && cell.row >= 0 && cell.row < rows;
 }
 
-Rectangle BoxToRec(Box box)
+void DrawTile(int row, int col, Color color)
 {
-    Rectangle rec;
-    rec.x = box.xMin;
-    rec.y = box.yMin;
-    rec.width = box.xMax - box.xMin;
-    rec.height = box.yMax - box.yMin;
-    return rec;
+    DrawRectangle(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE, color);
 }
 
-Box BallBox(Vector2 position)
+void DrawTile(int row, int col, int type)
 {
-    Box box;
-    box.xMin = position.x - BALL_SIZE * 0.5f;
-    box.xMax = position.x + BALL_SIZE * 0.5f;
-    box.yMin = position.y - BALL_SIZE * 0.5f;
-    box.yMax = position.y + BALL_SIZE * 0.5f;
-    return box;
+    Color color = type > 0 ? BEIGE : GREEN;
+    DrawTile(row, col, color);
 }
 
-Box PaddleBox(Vector2 position)
+Vector2 TileCenter(int row, int col)
 {
-    Box box;
-    box.xMin = position.x - PADDLE_WIDTH * 0.5f;
-    box.xMax = position.x + PADDLE_WIDTH * 0.5f;
-    box.yMin = position.y - PADDLE_HEIGHT * 0.5f;
-    box.yMax = position.y + PADDLE_HEIGHT * 0.5f;
-    return box;
+    float x = col * TILE_SIZE + TILE_SIZE * 0.5f;
+    float y = row * TILE_SIZE + TILE_SIZE * 0.5f;
+    return { x, y };
 }
 
-void ResetBall(Vector2& position, Vector2& direction)
+Vector2 TileCorner(int row, int col)
 {
-    position = CENTER;
-    direction.x = rand() % 2 == 0 ? -1.0f : 1.0f;
-    direction.y = 0.0f;
-    direction = Rotate(direction, Random(0.0f, 360.0f) * DEG2RAD);
+    float x = col * TILE_SIZE;
+    float y = row * TILE_SIZE;
+    return { x, y };
 }
 
-void DrawBall(Vector2 position, Color color)
+// Returns a collection of adjacent cells that match the search value.
+std::vector<Cell> FloodFill(Cell start, int tiles[TILE_COUNT][TILE_COUNT], TileType searchValue)
 {
-    Box ballBox = BallBox(position);
-    DrawRectangleRec(BoxToRec(ballBox), color);
+    // "open" = "places we want to search", "closed" = "places we've already searched".
+    std::vector<Cell> result;
+    std::vector<Cell> open;
+    bool closed[TILE_COUNT][TILE_COUNT];
+    for (int row = 0; row < TILE_COUNT; row++)
+    {
+        for (int col = 0; col < TILE_COUNT; col++)
+        {
+            // We don't want to search zero-tiles, so add them to closed!
+            closed[row][col] = tiles[row][col] == 0;
+        }
+    }
+
+    // Add the starting cell to the exploration queue & search till there's nothing left!
+    open.push_back(start);
+    while (!open.empty())
+    {
+        // Remove from queue and prevent revisiting
+        Cell cell = open.back();
+        open.pop_back();
+        closed[cell.row][cell.col] = true;
+
+        // Add to result if explored cell has the desired value
+        if (tiles[cell.row][cell.col] == searchValue)
+            result.push_back(cell);
+
+        // Search neighbours
+        for (Cell dir : DIRECTIONS)
+        {
+            Cell adj = { cell.row + dir.row, cell.col + dir.col };
+            if (InBounds(adj) && !closed[adj.row][adj.col] && tiles[adj.row][adj.col] > 0)
+                open.push_back(adj);
+        }
+    }
+
+    return result;
 }
 
-void DrawPaddle(Vector2 position, Color color)
+struct Bullet
 {
-    Box paddleBox = PaddleBox(position);
-    DrawRectangleRec(BoxToRec(paddleBox), color);
-}
+    Vector2 position{};
+    Vector2 direction{};
+    float time = 0.0f;
+    bool enabled = true;
+};
 
 int main()
 {
-    Vector2 ballPosition;
-    Vector2 ballDirection;
-    ResetBall(ballPosition, ballDirection);
+    int tiles[TILE_COUNT][TILE_COUNT]
+    {
+        //col:0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19    row:
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 }, // 0
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 1
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 2
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 3
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 4
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 5
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 6
+            { 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0 }, // 7
+            { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 8
+            { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 9
+            { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 10
+            { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 11
+            { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 12
+            { 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0 }, // 13
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, // 14
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, // 15
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, // 16
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0 }, // 17
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 18
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }  // 19
+    };
 
-    Vector2 paddle1Position, paddle2Position;
-    paddle1Position.x = SCREEN_WIDTH * 0.05f;
-    paddle2Position.x = SCREEN_WIDTH * 0.95f;
-    paddle1Position.y = paddle2Position.y = CENTER.y;
+    std::vector<Cell> waypoints = FloodFill({ 0, 12 }, tiles, WAYPOINT);
+    size_t curr = 0;
+    size_t next = curr + 1;
+    bool atEnd = false;
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong");
+    Vector2 enemyPosition = TileCenter(waypoints[curr].row, waypoints[curr].col);
+    const float enemySpeed = 250.0f;
+    const float enemyRadius = 20.0f;
+
+    const float bulletTime = 1.0f;
+    const float bulletSpeed = 500.0f;
+    const float bulletRadius = 15.0f;
+
+    std::vector<Bullet> bullets;
+    float shootCurrent = 0.0f;
+    float shootTotal = 0.25f;
+
+    InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Tower Defense");
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
-        float ballDelta = BALL_SPEED * dt;
-        float paddleDelta = PADDLE_SPEED * dt;
 
-        // Move paddle with key input
-        if (IsKeyDown(KEY_W))
-            paddle1Position.y -= paddleDelta;
-        if (IsKeyDown(KEY_S))
-            paddle1Position.y += paddleDelta;
+        // Path following
+        if (!atEnd)
+        {
+            Vector2 from = TileCenter(waypoints[curr].row, waypoints[curr].col);
+            Vector2 to = TileCenter(waypoints[next].row, waypoints[next].col);
+            Vector2 direction = Normalize(to - from);
+            enemyPosition = enemyPosition + direction * enemySpeed * dt;
+            if (CheckCollisionPointCircle(enemyPosition, to, enemyRadius))
+            {
+                curr++;
+                next++;
+                atEnd = next == waypoints.size();
+                enemyPosition = TileCenter(waypoints[curr].row, waypoints[curr].col);
+            }
+        }
 
-        // Mirror paddle 1 for now
-        paddle2Position.y = paddle1Position.y;
+        // Shooting
+        shootCurrent += dt;
+        if (shootCurrent >= shootTotal)
+        {
+            shootCurrent = 0.0f;
 
-        float phh = PADDLE_HEIGHT * 0.5f;
-        paddle1Position.y = Clamp(paddle1Position.y, phh, SCREEN_HEIGHT - phh);
-        paddle2Position.y = Clamp(paddle2Position.y, phh, SCREEN_HEIGHT - phh);
+            Bullet bullet;
+            bullet.position = GetMousePosition();
+            bullet.direction = Normalize(enemyPosition - bullet.position);
+            bullets.push_back(bullet);
+        }
 
-        // Change the ball's direction on-collision
-        Vector2 ballPositionNext = ballPosition + ballDirection * ballDelta;
-        Box ballBox = BallBox(ballPositionNext);
-        Box paddle1Box = PaddleBox(paddle1Position);
-        Box paddle2Box = PaddleBox(paddle2Position);
+        // Bullet update
+        for (Bullet& bullet : bullets)
+        {
+            bullet.position = bullet.position + bullet.direction * bulletSpeed * dt;
+            bullet.time += dt;
 
-        if (ballBox.xMin < 0.0f || ballBox.xMax > SCREEN_WIDTH)
-            ballDirection.x *= -1.0f;
-        if (ballBox.yMin < 0.0f || ballBox.yMax > SCREEN_HEIGHT)
-            ballDirection.y *= -1.0f;
-        if (BoxOverlap(ballBox, paddle1Box) || BoxOverlap(ballBox, paddle2Box))
-            ballDirection.x *= -1.0f;
+            bool expired = bullet.time >= bulletTime;
+            bool collision = CheckCollisionCircles(enemyPosition, enemyRadius, bullet.position, bulletRadius);
+            bullet.enabled = !expired && !collision;
+        }
 
-        // Update ball position after collision resolution, then render
-        ballPosition = ballPosition + ballDirection * ballDelta;
+        // Bullet removal
+        bullets.erase(std::remove_if(bullets.begin(), bullets.end(),
+            [&bullets](Bullet bullet) {
+                return !bullet.enabled;
+            }), bullets.end());
 
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawBall(ballPosition, WHITE);
-        DrawPaddle(paddle1Position, WHITE);
-        DrawPaddle(paddle2Position, WHITE);
+        for (int row = 0; row < TILE_COUNT; row++)
+        {
+            for (int col = 0; col < TILE_COUNT; col++)
+            {
+                DrawTile(row, col, tiles[row][col]);
+            }
+        }
+        DrawCircleV(enemyPosition, enemyRadius, RED);
+
+        // Render bullets
+        for (const Bullet& bullet : bullets)
+            DrawCircleV(bullet.position, bulletRadius, BLUE);
+        DrawText(TextFormat("Total bullets: %i", bullets.size()), 10, 10, 20, BLUE);
+
         EndDrawing();
     }
-
     CloseWindow();
     return 0;
 }
