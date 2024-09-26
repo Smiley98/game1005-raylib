263107b62b56b7244209a0b90444bc86a9558e40
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-26 18:27:39 -0400

    Added moving circle

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,17 @@ int main()
     // Vertical position of 50% of the screen height
     float y = GetScreenHeight() * 0.5f;
 
+    // Move at 100 pixels per second
+    float speed = 100.0f;
+
     while (!WindowShouldClose())
     {
+        // Time from previous to current frame (AKA Unity's Time.deltaTime)
+        float dt = GetFrameTime();
+
+        // Convert from 100 pixels per frame to 100 pixels per second, then increment x-positon
+        x += speed * dt;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawCircle(x, y, 25.0f, RED);
