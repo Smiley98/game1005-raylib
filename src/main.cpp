d9b894e8daef5b12da0bee5d413161653a24c98d
Smiley98 <smileysofficialcinema@gmail.com>
2024-10-03 18:06:21 -0400

    Added task 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "Math.h"
 #include <cassert>
+#include <array>
 #include <vector>
 
 constexpr int SCREEN_SIZE = 800;
@@ -21,6 +22,13 @@ struct Cell
     int col;
 };
 
+constexpr std::array<Cell, 4> DIRECTIONS{ Cell{ -1, 0 }, Cell{ 1, 0 }, Cell{ 0, -1 }, Cell{ 0, 1 } };
+
+inline bool InBounds(Cell cell, int rows = TILE_COUNT, int cols = TILE_COUNT)
+{
+    return cell.col >= 0 && cell.col < cols && cell.row >= 0 && cell.row < rows;
+}
+
 void DrawTile(int row, int col, Color color)
 {
     DrawRectangle(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE, color);
@@ -31,6 +39,47 @@ void DrawTile(int row, int col, int tileType)
     assert(tileType >= 0 && tileType < COUNT);
     Color colours[COUNT]{ LIME, BEIGE, SKYBLUE };
     DrawTile(row, col, colours[tileType]);
+}
+
+// Returns a collection of adjacent cells that match the search value.
+std::vector<Cell> FloodFill(Cell start, int tiles[TILE_COUNT][TILE_COUNT], TileType searchValue)
+{
+    // "open" = "places we want to search", "closed" = "places we've already searched".
+    std::vector<Cell> result;
+    std::vector<Cell> open;
+    bool closed[TILE_COUNT][TILE_COUNT];
+    for (int row = 0; row < TILE_COUNT; row++)
+    {
+        for (int col = 0; col < TILE_COUNT; col++)
+        {
+            // We don't want to search zero-tiles, so add them to closed!
+            closed[row][col] = tiles[row][col] == 0;
+        }
+    }
+
+    // Add the starting cell to the exploration queue & search till there's nothing left!
+    open.push_back(start);
+    while (!open.empty())
+    {
+        // Remove from queue and prevent revisiting
+        Cell cell = open.back();
+        open.pop_back();
+        closed[cell.row][cell.col] = true;
+
+        // Add to result if explored cell has the desired value
+        if (tiles[cell.row][cell.col] == searchValue)
+            result.push_back(cell);
+
+        // Search neighbours
+        for (Cell dir : DIRECTIONS)
+        {
+            Cell adj = { cell.row + dir.row, cell.col + dir.col };
+            if (InBounds(adj) && !closed[adj.row][adj.col] && tiles[adj.row][adj.col] > 0)
+                open.push_back(adj);
+        }
+    }
+
+    return result;
 }
 
 int main()
@@ -60,15 +109,21 @@ int main()
         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }  // 19
     };
 
-    std::vector<Cell> waypoints;
-    waypoints.push_back({ 0, 12 });
-    waypoints.push_back({ 7, 12 });
-    waypoints.push_back({ 7, 3 });
-    waypoints.push_back({ 13, 3 });
-    waypoints.push_back({ 13, 16 });
-    waypoints.push_back({ 17, 16 });
-    waypoints.push_back({ 17, 9 });
-    waypoints.push_back({ 19, 9 });
+    // Manual approach:
+    //std::vector<Cell> waypoints;
+    //waypoints.push_back({ 0, 12 });
+    //waypoints.push_back({ 7, 12 });
+    //waypoints.push_back({ 7, 3 });
+    //waypoints.push_back({ 13, 3 });
+    //waypoints.push_back({ 13, 16 });
+    //waypoints.push_back({ 17, 16 });
+    //waypoints.push_back({ 17, 9 });
+    //waypoints.push_back({ 19, 9 });
+    
+    // Automatic approach:
+    std::vector<Cell> waypoints = FloodFill({ 0, 13 }, tiles, GRASS);
+
+    // Task 1: Store a vector of dirt cells called path. Render all path cells in a colour of your choice!
 
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
