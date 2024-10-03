c249a27163f04b19b803baf19f0eabaa2f62e299
Smiley98 <smileysofficialcinema@gmail.com>
2024-10-03 15:09:03 -0400

    Added raygui

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 #include "Math.h"
 
 int main()
 {
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
+    bool showMessageBox = false;
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 10, 10, 20, GRAY);
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+        if (GuiButton({ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+
+        if (showMessageBox)
+        {
+            int result = GuiMessageBox( { 85, 70, 250, 100 },
+                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+            if (result >= 0) showMessageBox = false;
+        }
+
         EndDrawing();
     }
 
