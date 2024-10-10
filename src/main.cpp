a0d512818e47102f6aa4e465727716ea2a4ceb53
Smiley98 <smileysofficialcinema@gmail.com>
2024-10-10 17:18:32 -0400

    Added state machine base

--- src/main.cpp
+++ src/main.cpp
@@ -5,164 +5,92 @@ constexpr float SCREEN_WIDTH = 1200.0f;
 constexpr float SCREEN_HEIGHT = 800.0f;
 constexpr Vector2 CENTER{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
 
-// Ball can move half the screen width per-second
-constexpr float BALL_SPEED = SCREEN_WIDTH * 0.5f;
-constexpr float BALL_SIZE = 40.0f;
-
-// Paddles can move half the screen height per-second
-constexpr float PADDLE_SPEED = SCREEN_HEIGHT * 0.5f;
-constexpr float PADDLE_WIDTH = 40.0f;
-constexpr float PADDLE_HEIGHT = 80.0f;
-
-struct Box
+struct Game
 {
-    float xMin;
-    float xMax;
-    float yMin;
-    float yMax;
+
 };
 
-bool BoxOverlap(Box box1, Box box2)
+enum State : int
 {
-    bool x = box1.xMax >= box2.xMin && box1.xMin <= box2.xMax;
-    bool y = box1.yMax >= box2.yMin && box1.yMin <= box2.yMax;
-    return x && y;
+    BEGIN,
+    PLAY,
+    END
+};
+
+void UpdateBegin(Game& game)
+{
+
 }
 
-Rectangle BoxToRec(Box box)
+void UpdatePlay(Game& game)
 {
-    Rectangle rec;
-    rec.x = box.xMin;
-    rec.y = box.yMin;
-    rec.width = box.xMax - box.xMin;
-    rec.height = box.yMax - box.yMin;
-    return rec;
+
 }
 
-Box BallBox(Vector2 position)
+void UpdateEnd(Game& game)
 {
-    Box box;
-    box.xMin = position.x - BALL_SIZE * 0.5f;
-    box.xMax = position.x + BALL_SIZE * 0.5f;
-    box.yMin = position.y - BALL_SIZE * 0.5f;
-    box.yMax = position.y + BALL_SIZE * 0.5f;
-    return box;
+
 }
 
-Box PaddleBox(Vector2 position)
+void DrawBegin(Game& game)
 {
-    Box box;
-    box.xMin = position.x - PADDLE_WIDTH * 0.5f;
-    box.xMax = position.x + PADDLE_WIDTH * 0.5f;
-    box.yMin = position.y - PADDLE_HEIGHT * 0.5f;
-    box.yMax = position.y + PADDLE_HEIGHT * 0.5f;
-    return box;
+    DrawRectangle(550, 350, 100, 50, RED);
 }
 
-void ResetBall(Vector2& position, Vector2& direction)
+void DrawPlay(Game& game)
 {
-    position = CENTER;
-    direction.x = rand() % 2 == 0 ? -1.0f : 1.0f;
-    direction.y = 0.0f;
-    direction = Rotate(direction, Random(0.0f, 360.0f) * DEG2RAD);
+    DrawRectangle(550, 350, 100, 50, GREEN);
 }
 
-void DrawBall(Vector2 position, Color color)
+void DrawEnd(Game& game)
 {
-    Box ballBox = BallBox(position);
-    DrawRectangleRec(BoxToRec(ballBox), color);
-}
-
-void DrawPaddle(Vector2 position, Color color)
-{
-    Box paddleBox = PaddleBox(position);
-    DrawRectangleRec(BoxToRec(paddleBox), color);
+    DrawRectangle(550, 350, 100, 50, BLUE);
 }
 
 int main()
 {
-    Vector2 ballPosition;
-    Vector2 ballDirection;
-    ResetBall(ballPosition, ballDirection);
-
-    Vector2 paddle1Position, paddle2Position;
-    paddle1Position.x = SCREEN_WIDTH * 0.05f;
-    paddle2Position.x = SCREEN_WIDTH * 0.95f;
-    paddle1Position.y = paddle2Position.y = CENTER.y;
-
-    int testScore = 0;
-    int score = 0;
-
-    bool gameOver = false;
+    Game game;
+    int state = BEGIN;
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong");
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        gameOver = score >= 2;
-
-        float dt = GetFrameTime();
-        float ballDelta = BALL_SPEED * dt;
-        float paddleDelta = PADDLE_SPEED * dt;
-
-        // Move paddle with key input
-        if (IsKeyDown(KEY_W))
-            paddle1Position.y -= paddleDelta;
-        if (IsKeyDown(KEY_S))
-            paddle1Position.y += paddleDelta;
-
-        // Mirror paddle 1 for now
-        paddle2Position.y = paddle1Position.y;
-
-        float phh = PADDLE_HEIGHT * 0.5f;
-        paddle1Position.y = Clamp(paddle1Position.y, phh, SCREEN_HEIGHT - phh);
-        paddle2Position.y = Clamp(paddle2Position.y, phh, SCREEN_HEIGHT - phh);
-
-        // Change the ball's direction on-collision
-        Vector2 ballPositionNext = ballPosition + ballDirection * ballDelta;
-        Box ballBox = BallBox(ballPositionNext);
-        Box paddle1Box = PaddleBox(paddle1Position);
-        Box paddle2Box = PaddleBox(paddle2Position);
-
-        // TODO -- increment the scoring player's score after they've touched the ball and the ball goes too far right/left
-        testScore++;
-        if (ballBox.xMin < 0.0f || ballBox.xMax > SCREEN_WIDTH)
+        // State change test
+        if (IsKeyPressed(KEY_SPACE))
         {
-            ballDirection.x *= -1.0f;
-            score++;
-        }
-        if (ballBox.yMin < 0.0f || ballBox.yMax > SCREEN_HEIGHT)
-        {
-            ballDirection.y *= -1.0f;
-            score++;
-        }
-        if (BoxOverlap(ballBox, paddle1Box) || BoxOverlap(ballBox, paddle2Box))
-        {
-            ballDirection.x *= -1.0f;
+            ++state %= 3;
         }
 
-        // Update ball position after collision resolution, then render
-        ballPosition = ballPosition + ballDirection * ballDelta;
+        switch (state)
+        {
+        case BEGIN:
+            UpdateBegin(game);
+            break;
+
+        case PLAY:
+            UpdatePlay(game);
+            break;
+
+        case END:
+            UpdateEnd(game);
+            break;
+        }
 
         BeginDrawing();
-        if (gameOver)
+        switch (state)
         {
-            ClearBackground(RED);
-            DrawText("You lost :(", 10, 10, 20, BLACK);
-        }
-        else
-        {
-            ClearBackground(BLACK);
-            DrawBall(ballPosition, WHITE);
-            DrawPaddle(paddle1Position, WHITE);
-            DrawPaddle(paddle2Position, WHITE);
+        case BEGIN:
+            DrawBegin(game);
+            break;
 
-            // Text format requires you to put a '%i' wherever you want an integer, then add said integer after the comma
-            const char* testScoreText = TextFormat("Test Score: %i ", testScore);
+        case PLAY:
+            DrawPlay(game);
+            break;
 
-            // We can measure our text for more exact positioning. This puts our score in the center of our screen!
-            DrawText(testScoreText, SCREEN_WIDTH * 0.5f - MeasureText(testScoreText, 20) * 0.5f, 50, 20, BLUE);
+        case END:
+            DrawEnd(game);
+            break;
         }
-        
         EndDrawing();
     }
 
