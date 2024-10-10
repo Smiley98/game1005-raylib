3fe5757554b37a64eaac59b2293f5a94314e8d42
Smiley98 <smileysofficialcinema@gmail.com>
2024-10-10 17:29:11 -0400

    Added state-based buttons

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,6 @@ constexpr float SCREEN_WIDTH = 1200.0f;
 constexpr float SCREEN_HEIGHT = 800.0f;
 constexpr Vector2 CENTER{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
 
-struct Game
-{
-
-};
-
 enum State : int
 {
     BEGIN,
@@ -17,40 +12,62 @@ enum State : int
     END
 };
 
+struct Game
+{
+    Rectangle button;
+    Color buttonColor;
+    int state;
+};
+
 void UpdateBegin(Game& game)
 {
-
+    // Add mouse-out vs mouse-over colour
+    Color buttonColorIn = GRAY;
+    Color buttonColorOut = DARKGRAY;
+    bool mouseOver = CheckCollisionPointRec(GetMousePosition(), game.button);
+    game.buttonColor = mouseOver ? buttonColorIn : buttonColorOut;
+    if (mouseOver && IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+        ++game.state %= 3;
 }
 
 void UpdatePlay(Game& game)
 {
-
+    // Replace this with actual win condition to transition from pong play to pong game over
+    if (CheckCollisionPointRec(GetMousePosition(), game.button) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+        ++game.state %= 3;
 }
 
 void UpdateEnd(Game& game)
 {
-
+    // Replace with a play again vs quit option?
+    if (CheckCollisionPointRec(GetMousePosition(), game.button) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+        ++game.state %= 3;
 }
 
 void DrawBegin(Game& game)
 {
-    DrawRectangle(550, 350, 100, 50, RED);
+    DrawRectangleRec(game.button, game.buttonColor);
 }
 
 void DrawPlay(Game& game)
 {
-    DrawRectangle(550, 350, 100, 50, GREEN);
+    DrawRectangleRec(game.button, GREEN);
 }
 
 void DrawEnd(Game& game)
 {
-    DrawRectangle(550, 350, 100, 50, BLUE);
+    DrawRectangleRec(game.button, BLUE);
 }
 
 int main()
 {
     Game game;
-    int state = BEGIN;
+    game.state = BEGIN;
+    game.button.x = 550;
+    game.button.y = 350;
+    game.button.width = 100;
+    game.button.height = 50;
+
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong");
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -58,10 +75,10 @@ int main()
         // State change test
         if (IsKeyPressed(KEY_SPACE))
         {
-            ++state %= 3;
+            ++game.state %= 3;
         }
 
-        switch (state)
+        switch (game.state)
         {
         case BEGIN:
             UpdateBegin(game);
@@ -77,7 +94,7 @@ int main()
         }
 
         BeginDrawing();
-        switch (state)
+        switch (game.state)
         {
         case BEGIN:
             DrawBegin(game);
